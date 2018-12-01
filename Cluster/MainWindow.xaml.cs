8b81158f67a80d33b5e631528ecc4c873742cac7
Dovydas <Dovydas@DESKTOP-LR9DMUL>
2018-12-01 23:53:26 +0200

    More progress on part 2.

--- Cluster/MainWindow.xaml.cs
+++ Cluster/MainWindow.xaml.cs
@@ -21,9 +21,9 @@ namespace Cluster
     /// </summary>
     public partial class MainWindow : Window
     {
-        const double WCF = 308_525.05; // warehouse construction cost fixed      eur
+        const double WCF = 308525.05;  // warehouse construction cost fixed      eur
         const double WCV = 539.91;     // warehouse construction cost variable   eur/ton
-        const double WMF = 8_513.26;   // warehouse management cost fixed        eur/month
+        const double WMF = 8513.26;    // warehouse management cost fixed        eur/month
         const double WMV = 6.31;       // warehouse management cost variable     eur/ton/day
         const double TD = 125.41;      // truck delivery cost                    eur/ton/km
         const double RD = 3.95;        // railway delivery cost                  eur/ton/km
@@ -58,9 +58,9 @@ namespace Cluster
             Console.WriteLine("{0} \n{1}", railCost, roadCost);
 
             //Grafo testas
-
             Graph<string> graph = new Graph<string>();
             graph = CreateGraph(distances, roadFlow, railFlow);
+
             using (StreamWriter writer = new StreamWriter("Test.txt"))
             {
                 foreach (GraphNode<string> node in graph.Nodes)
@@ -69,10 +69,16 @@ namespace Cluster
                     writer.WriteLine(node.Value + " kaimynai:");
                     foreach (GraphNode<string> neib in node.Neighbors)
                     {
-                        writer.WriteLine(neib.Value + " RoadFlow cost -" + neib.RoadCosts[i] + "RailFlow cost -" + neib.RailCosts[i]);
+                        writer.WriteLine(neib.Value + " RoadFlow cost ->" + neib.RoadCosts[i] + " RailFlow cost ->" + neib.RailCosts[i]);
+                        i++;
                     }
                 }
             }
+            // Pigiausio kelio testinimas
+            graph.FindByValue("AT12").IsWarehouse = true;
+            graph.FindByValue("AT11").IsWarehouse = true;
+            // Kazkodel grazina atvirkscia reiksme (vietoj kelio AT11 -> AT12, grazina AT12 -> AT11)
+            FindCheapestPath(graph.FindByValue("AT11"), graph.FindByValue("AT12"), graph);
         }
 
         Graph<string> CreateGraph(HashSet<Distance> distances, HashSet<Flow> roadFlows, HashSet<Flow> railFlows)
@@ -100,6 +106,7 @@ namespace Cluster
             }
             return graph;
         }
+
         //Reikia dar optimizuoti
         //kai IsRoadFlow = true skaiciuoja gabenimo sunkvezimiu kastus, false kai traukiniu
         double TranspCostCalc(double distance, HashSet<Flow> roadFlow, HashSet<Flow> railFlow, GraphNode<string> from, GraphNode<string> to, bool IsRoadFlow)
@@ -108,6 +115,7 @@ namespace Cluster
             double totalCost = 0;
             if (IsRoadFlow)
             {
+                //Flow road = roadFlow.FirstOrDefault(x => x.Load.Equals(from.Value) && x.Unload.Equals(to.Value));
                 foreach (var item in roadFlow)
                 {
                     if (item.Load == from.Value && item.Unload == to.Value)
@@ -116,12 +124,16 @@ namespace Cluster
                         break;
                     }
                 }
-
-                totalCost = distance * TE + transportationCost;
+                /*if (road != null)
+                    transportationCost = road.FlowTonKMs * TD;*/
+                if (transportationCost > 0)
+                    totalCost = distance * TE + transportationCost;
+                
                 return totalCost;
             }
             else
             {
+                //Flow rail = railFlow.FirstOrDefault(x => x.Load.Equals(from.Value) && x.Unload.Equals(to.Value));
                 foreach (var item in railFlow)
                 {
                     if (item.Load == from.Value && item.Unload == to.Value)
@@ -130,10 +142,52 @@ namespace Cluster
                         break;
                     }
                 }
+                /*if (rail != null)
+                    transportationCost = rail.FlowTonKMs * RD;*/
+                if (transportationCost > 0)
+                    totalCost = distance * RE + transportationCost;
 
-                totalCost = distance * RE + transportationCost;
                 return totalCost;
             }
+        }
+
+        void FindCheapestPath(GraphNode<string> Origin, GraphNode<string> Destination, Graph<string> graph)
+        {
+            // jei abi virsunes sandeliai, tai tiesiog tikrinam ar pigiau su traukiniu gabent ar su sunkvezimiu
+            if(Origin.IsWarehouse && Destination.IsWarehouse)
+            {
+                int key = 999999;
+                Console.WriteLine("From " + Origin.Value + " To " + Destination.Value);
+                for (int i = 0; i < Origin.Neighbors.Count; i++)
+                {
+                    if (Origin.Neighbors[i].Value == Destination.Value)
+                    {
+                        key = i;
+                        break;
+                    }
+                }
+
+                if (graph.FindByValue(Origin.Value).RailCosts[key] < graph.FindByValue(Origin.Value).RoadCosts[key])
+                    Console.WriteLine("Cheaper by truck -> " + graph.FindByValue(Origin.Value).RoadCosts[key]);
+                else
+                    Console.WriteLine("Cheaper by train -> " + graph.FindByValue(Origin.Value).RailCosts[key]);
+            }
+            // jei pradzia sandelys o pabaiga ne, reikia ieskot ar bent vienas is pabaigos kaimynu yra sandelys, jeigu taip, tikrinti ar 
+            // imanoma is to sandelio nueit i pradzios sandeli, jeigu imanoma - sumuojam kaina traukinys+sunkvezimis(iki galutinio tasko)
+            // ir lyginam su tiesioginio vezimo sunkvezimiu kaina is pradzios i galutini taska ir grazinam pigesni varianta, jei kuris nors
+            // is ankstesniu if'u nepasiteisina, grazinam tiesioginio vezimo sunkvezimiu kaina is pradzios i galutini taska.
+
+            // jei pradzia nera sandelys, o pabaiga yra, reikia ieskoti ar bent vienas is pradzios kaimynu yra sandelys, jeigu taip, tikrinti ar 
+            // imanoma is to sandelio nueit i pabaigos sandeli, jeigu imanoma - sumuojam kaina sunkvezimis(iki tarpinio sandelio)+traukinys(iki tikslo)
+            // ir lyginam su tiesioginio vezimo sunkvezimiu kaina is pradzios i galutini taska ir grazinam pigesni varianta, jei kuris nors
+            // is ankstesniu if'u nepasiteisina, grazinam tiesioginio vezimo sunkvezimiu kaina is pradzios i galutini taska.
+
+            // jei nei pradzia nei pabaiga nera sandelys, issirenkame ju kaimynus kurie yra sandeliai, tada tikriname ar is kaimyniniu pradzios
+            // sandeliu imanoma nuvaziuot traukiniu i kaimyninius pabaigos sandelius, jeigu imanoma surandame visu galimu kelioniu kainas
+            // sunkvezimis+traukinys+sunkvezimis ir isrenkame maziausia, tada lyginame su tiesioginio vezimo sunkvezimiu kaina is pradzios i 
+            // galutini taska ir grazinam pigesni varianta, jei kuris nors is ankstesniu if'u nepasiteisina, grazinam tiesioginio vezimo sunkvezimiu kaina.
+
+            // ir aisku paskutinis "else" grazina tiesioginio vezimo sunkvezimiu kaina.
         }
     }
 }
